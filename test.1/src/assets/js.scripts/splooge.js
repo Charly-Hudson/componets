082035f3754ef2c6c51b750e35afcc6d8af127a9
Charly-Hudson <charlydhudson+Github@gmail.com>
2024-08-07 14:13:23 +0100

    Fix for nave and ordering

--- test.1/src/assets/js.scripts/splooge.js
+++ test.1/src/assets/js.scripts/splooge.js
@@ -1,10 +1,1 @@
 // a function that removes a css component
-
-document.addEventListener('scroll', () => {
-    const scroll = document.documentElement.scrollTop;
-    if (scroll >= 100) {
-        document.querySelector('.splooge')?.classList.add('scroll');
-    } else {
-        document.querySelector('.splooge')?.classList.remove('scroll');
-    }
-});
